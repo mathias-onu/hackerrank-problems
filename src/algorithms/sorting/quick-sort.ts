6bdbceef842128c86f272ab4d7b9950d58f3c827
mathias-onu <mathias.onu@outlook.com>
2024-03-21 14:48:09 +0200

    added another 2 important sorting algorithms

---
+++ src/algorithms/sorting/quick-sort.ts
@@ -0,0 +1,18 @@
+import { generateArray } from "./util"
+
+const quickSort = (arr: number[]): number[] => {
+    if (arr.length < 2) return arr
+
+    const pivot = arr[arr.length - 1]
+    const left = []
+    const right = []
+    
+    for (let i = 0; i < arr.length -1; i++) {
+        if (arr[i] < pivot) left.push(arr[i])
+        else right.push(arr[i])
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)]
+}
+
+console.log(quickSort(generateArray(10)))
